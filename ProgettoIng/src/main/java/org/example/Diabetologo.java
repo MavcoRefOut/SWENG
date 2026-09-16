c2c85670b186df7cca066678a4805a42a03eaacf
GiacomoM413 <132558826+Gieck413@users.noreply.github.com>
2026-09-16 15:35:42 +0200

    Update Diabetologo.java

--- ProgettoIng/src/main/java/org/example/Diabetologo.java
+++ ProgettoIng/src/main/java/org/example/Diabetologo.java
@@ -3,10 +3,13 @@ package org.example;
 import java.util.Date;
 //classe che descrive un diabetologo
 public class Diabetologo extends Persona{
+
+    //costruttore con parametri della classe, richiama quello di Persona
     public Diabetologo(String codiceFiscale, String nome, String cognome, Date dataNascita, String email, String username, String password) {
         super(codiceFiscale, nome, cognome, dataNascita, email, username, password);
     }
 
+    //costruttore di default della classe
     public Diabetologo(){
         super();
     }
