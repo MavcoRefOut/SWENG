307b9c34faf109eb163a9e0e4e41ab963a7fd59f
GiacomoM413 <132558826+Gieck413@users.noreply.github.com>
2026-09-16 15:29:19 +0200

    Update CreatoreSegnalazione.java

--- ProgettoIng/src/main/java/org/example/CreatoreSegnalazione.java
+++ ProgettoIng/src/main/java/org/example/CreatoreSegnalazione.java
@@ -1,7 +1,7 @@
 package org.example;
 
 import java.time.LocalDate;
-
+//abstract factory per generare fabbriche di segnalazioni
 public abstract class CreatoreSegnalazione {
     public abstract Segnalazione factoryMethod(
             LocalDate dataInizio,
