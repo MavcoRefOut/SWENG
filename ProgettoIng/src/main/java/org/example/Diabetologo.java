951e1b6a13aca2b4af1f731c81f1eba04d292af1
GiacomoM413 <132558826+Gieck413@users.noreply.github.com>
2026-09-16 15:31:11 +0200

    Update Diabetologo.java

--- ProgettoIng/src/main/java/org/example/Diabetologo.java
+++ ProgettoIng/src/main/java/org/example/Diabetologo.java
@@ -1,7 +1,7 @@
 package org.example;
 
 import java.util.Date;
-
+//classe che descrive un diabetologo
 public class Diabetologo extends Persona{
     public Diabetologo(String codiceFiscale, String nome, String cognome, Date dataNascita, String email, String username, String password) {
         super(codiceFiscale, nome, cognome, dataNascita, email, username, password);
