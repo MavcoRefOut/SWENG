8cd3bc3ce40dc204c69624e91a343f579406aa34
GiacomoM413 <132558826+Gieck413@users.noreply.github.com>
2026-09-16 16:17:31 +0200

    Update Notifica.java

--- ProgettoIng/src/main/java/org/example/Notifica.java
+++ ProgettoIng/src/main/java/org/example/Notifica.java
@@ -2,12 +2,14 @@ package org.example;
 
 import java.io.IOException;
 
+//classe che descrive una notifica
 public class Notifica {
     private Persona mandante;
     private Persona destinatario;
     private String messaggio;
     private LivelloPericolo livelloPericolo;
 
+    //costruttore con parametri della classe
     public Notifica(Persona mandante, Persona destinatario, String messaggio, LivelloPericolo livelloPericolo) {
         this.mandante = mandante;
         this.destinatario = destinatario;
@@ -15,44 +17,54 @@ public class Notifica {
         this.livelloPericolo = livelloPericolo;
     }
 
+    //getter del campo della classe omonimo
     public Persona getMandante() {
         return mandante;
     }
 
+    //setter del campo della classe omonimo
     public void setMandante(Persona mandante) {
         this.mandante = mandante;
     }
 
+    //getter del campo della classe omonimo
     public Persona getDestinatario() {
         return destinatario;
     }
 
+    //setter del campo della classe omonimo
     public void setDestinatario(Persona destinatario) {
         this.destinatario = destinatario;
     }
 
+    //getter del campo della classe omonimo
     public String getMessaggio() {
         return messaggio;
     }
 
+    //setter del campo della classe omonimo
     public void setMessaggio(String messaggio) {
         this.messaggio = messaggio;
     }
 
+    //getter del campo della classe omonimo
     public LivelloPericolo getLivelloPericolo() {
         return livelloPericolo;
     }
 
+    //setter del campo della classe omonimo
     public void setLivelloPericolo(LivelloPericolo livelloPericolo) {
         this.livelloPericolo = livelloPericolo;
     }
 
+    //metodo che scrive una notifica sul file 'inviandola'
     public static Boolean inviaNotifica(Notifica notifica) throws IOException {
         GestoreFile gestoreFile = new GestoreFile();
         gestoreFile.scriviNotifica(notifica);
         return true;
     }
 
+    //override del metodo toString adattato alla classe
     @Override
     public String toString() {
         String cfMandante = (this.getMandante() != null) ? this.getMandante().getCodiceFiscale() : "N/D";
