b1dee3131255cc5eb017165761dfc9a71b5b509b
GiacomoM413 <132558826+Gieck413@users.noreply.github.com>
2026-09-16 15:37:47 +0200

    Update Farmaco.java

--- ProgettoIng/src/main/java/org/example/Farmaco.java
+++ ProgettoIng/src/main/java/org/example/Farmaco.java
@@ -1,32 +1,40 @@
 package org.example;
 
+//classe che descrive un farmaco
 public class Farmaco {
     private String nomeFarmaco;
     private String principioAttivo;
 
+    //costruttore di default di un farmaco
     public Farmaco(){}
 
+    //costruttore con parametri della classe
     public Farmaco(String nomeFarmaco, String principioAttivo){
         this.nomeFarmaco = nomeFarmaco;
         this.principioAttivo = principioAttivo;
     }
 
+    //getter del campo della classe omonimo
     public String getNomeFarmaco() {
         return nomeFarmaco;
     }
 
+    //setter del campo della classe omonimo
     public void setNomeFarmaco(String nomeFarmaco) {
         this.nomeFarmaco = nomeFarmaco;
     }
 
+    //getter del campo della classe omonimo
     public String getPrincipioAttivo() {
         return principioAttivo;
     }
 
+    //setter del campo della classe omonimo
     public void setPrincipioAttivo(String principioAttivo) {
         this.principioAttivo = principioAttivo;
     }
 
+    //override di equals per verificare se due farmaci confrontati corrispondono in base al nome commerciale
     @Override
     public boolean equals(Object o) {
 
@@ -41,6 +49,7 @@ public class Farmaco {
         return this.nomeFarmaco.equalsIgnoreCase(farmaco.nomeFarmaco);
     }
 
+    //override del metodo per generare l'hash dell'oggetto farmaco
     @Override
     public int hashCode() {
         return nomeFarmaco != null ? nomeFarmaco.toLowerCase().hashCode() : 0;
