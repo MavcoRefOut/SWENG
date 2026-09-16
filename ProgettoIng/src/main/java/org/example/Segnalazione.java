aa6d0de668fe8db10aae959cc7e27ae45e5c2396
GiacomoM413 <132558826+Gieck413@users.noreply.github.com>
2026-09-16 16:35:09 +0200

    Update Segnalazione.java

--- ProgettoIng/src/main/java/org/example/Segnalazione.java
+++ ProgettoIng/src/main/java/org/example/Segnalazione.java
@@ -16,15 +16,18 @@ import java.time.LocalDate;
         @JsonSubTypes.Type(value = TerapiaConcomitante.class, name = "TerapiaConcomitante")
 })
 
+//classe astratta che descrive una segnalazione
 public abstract class Segnalazione {
 
     private LocalDate dataInizio;
     private LocalDate dataFine; // Può essere null se la condizione/terapia è ancora in corso
     private String descrizione;
 
+        //costruttore di default della classe
     public Segnalazione() {
     }
 
+        //costruttore con parametri della classe, controlla la validità delle date
     public Segnalazione(LocalDate dataInizio, LocalDate dataFine, String descrizione) {
         if (dataInizio == null) {
             throw new IllegalArgumentException("La data di inizio non può essere null.");
@@ -37,16 +40,18 @@ public abstract class Segnalazione {
         this.descrizione = descrizione;
     }
 
-
+        //ritorna un boolean che ci dice se la segnalazione è in corso o meno
     public boolean inCorso() {
         LocalDate oggi = LocalDate.now();
         return this.dataFine == null || !this.dataFine.isBefore(oggi);
     }
 
+        //getter del campo della classe omonimo
     public LocalDate getDataInizio() {
         return dataInizio;
     }
 
+        //setter del campo della classe omonimo
     public void setDataInizio(LocalDate dataInizio) {
         if (dataInizio == null) {
             throw new IllegalArgumentException("La data di inizio non può essere null.");
@@ -57,10 +62,12 @@ public abstract class Segnalazione {
         this.dataInizio = dataInizio;
     }
 
+        //getter del campo della classe omonimo
     public LocalDate getDataFine() {
         return dataFine;
     }
 
+        //setter del campo della classe omonimo
     public void setDataFine(LocalDate dataFine) {
         if (dataFine != null && this.dataInizio != null && dataFine.isBefore(this.dataInizio)) {
             throw new IllegalArgumentException("La data di fine non può essere precedente alla data di inizio.");
@@ -68,6 +75,7 @@ public abstract class Segnalazione {
         this.dataFine = dataFine;
     }
 
+        //getter del campo della classe omonimo
     public String getDescrizione() {
         return descrizione;
     }
