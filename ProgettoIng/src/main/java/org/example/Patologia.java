2b937e560cc4ac9b2aaa6dae5ad69df1edadaa1f
GiacomoM413 <132558826+Gieck413@users.noreply.github.com>
2026-09-16 16:19:28 +0200

    Update Patologia.java

--- ProgettoIng/src/main/java/org/example/Patologia.java
+++ ProgettoIng/src/main/java/org/example/Patologia.java
@@ -2,15 +2,16 @@ package org.example;
 
 import java.time.LocalDate;
 
+//classe che descrive una patologia
 public class Patologia extends Segnalazione{
     private String nome;
 
-
+    //costruttore di default della classe, usa quello di Segnalazione
     public Patologia() {
         super();
     }
 
-
+    //costruttore con parametri della classe, usa quello di Segnalazione e controlla il nome della patologia inserito
     public Patologia(LocalDate dataInizio, LocalDate dataFine, String note, String nome) {
         super(dataInizio, dataFine, note);
         if (nome == null || nome.trim().isEmpty()) {
@@ -19,14 +20,17 @@ public class Patologia extends Segnalazione{
         this.nome = nome.trim();
     }
 
+    //getter del campo della classe omonimo
     public String getDescrizione() {
         return this.nome;
     }
 
+    //getter del campo della classe omonimo
     public String getNome() {
         return nome;
     }
 
+    //setter del campo della classe omonimo
     public void setNome(String nome) {
         if (nome == null || nome.trim().isEmpty()) {
             throw new IllegalArgumentException("Il nome della patologia non può essere vuoto.");
@@ -34,6 +38,7 @@ public class Patologia extends Segnalazione{
         this.nome = nome.trim();
     }
 
+    //override del metodo toString adattato alla classe
     @Override
     public String toString() {
         return "Patologia" +
