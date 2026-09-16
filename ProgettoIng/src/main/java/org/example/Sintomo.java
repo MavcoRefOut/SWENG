9b7b64a3fcfcd8dc39ccbb4dd5432f103f7779e7
GiacomoM413 <132558826+Gieck413@users.noreply.github.com>
2026-09-16 16:36:44 +0200

    Update Sintomo.java

--- ProgettoIng/src/main/java/org/example/Sintomo.java
+++ ProgettoIng/src/main/java/org/example/Sintomo.java
@@ -2,25 +2,33 @@ package org.example;
 
 import java.time.LocalDate;
 
+//classe che descrive un sintomo
 public class Sintomo extends Segnalazione {
 
+    private String nome;
+    
+    //costruttore di default della classe, usa quello di Segnalazione
     public Sintomo(){
         super();
     }
 
+    //costruttore con parametri della classe, usa quello di Segnalazione
     public Sintomo(LocalDate dataInizio, LocalDate dataFine, String descrizione, String nome){
         super(dataInizio,dataFine,descrizione);
         this.nome = nome;
     }
 
-    private String nome;
+    //getter del campo della classe omonimo
     public String getNome() {
         return nome;
     }
+
+    //setter del campo della classe omonimo
     public void setNome(String nome) {
         this.nome = nome.trim();
     }
 
+    //override del metodo toString adattato alla classe
     @Override
     public String toString() {
         return "Sintomo" +
