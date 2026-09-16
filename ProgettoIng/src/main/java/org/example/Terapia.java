c66c3de8f82bc15867c0027421f1ea0246ca9c1f
GiacomoM413 <132558826+Gieck413@users.noreply.github.com>
2026-09-16 16:38:38 +0200

    Update Terapia.java

--- ProgettoIng/src/main/java/org/example/Terapia.java
+++ ProgettoIng/src/main/java/org/example/Terapia.java
@@ -1,5 +1,6 @@
 package org.example;
 
+//classe che descrive una terapia
 public class Terapia {
     private int assunzioniGG;
     private String indicazioni;
@@ -7,10 +8,11 @@ public class Terapia {
     private Farmaco farmaco;
     private Diabetologo medicoPrescrittore;
 
-
+    //cotruttore di default della classe
     public Terapia() {
     }
 
+    //cotruttore con parametri della classe
     public Terapia(int assunzioniGG, String indicazioni, int quantita, Farmaco farmaco, Diabetologo medicoPrescrittore) {
         this.assunzioniGG = assunzioniGG;
         this.indicazioni = indicazioni;
@@ -19,46 +21,57 @@ public class Terapia {
         this.medicoPrescrittore = medicoPrescrittore;
     }
 
+    //getter del campo della classe omonimo
     public int getAssunzioniGG() {
         return assunzioniGG;
     }
 
+    //setter del campo della classe omonimo
     public void setAssunzioniGG(int assunzioniGG) {
         this.assunzioniGG = assunzioniGG;
     }
 
+    //getter del campo della classe omonimo
     public String getIndicazioni() {
         return indicazioni;
     }
 
+    //setter del campo della classe omonimo
     public void setIndicazioni(String indicazioni) {
         this.indicazioni = indicazioni;
     }
 
+    //getter del campo della classe omonimo
     public int getQuantita() {
         return quantita;
     }
 
+    //setter del campo della classe omonimo
     public void setQuantita(int quantita) {
         this.quantita = quantita;
     }
 
+    //getter del campo della classe omonimo
     public Farmaco getFarmaco() {
         return farmaco;
     }
 
+    //setter del campo della classe omonimo
     public void setFarmaco(Farmaco farmaco) {
         this.farmaco = farmaco;
     }
 
+    //getter del campo della classe omonimo
     public Diabetologo getMedicoPrescrittore() {
         return medicoPrescrittore;
     }
 
+    //setter del campo della classe omonimo
     public void setMedicoPrescrittore(Diabetologo medicoPrescrittore) {
         this.medicoPrescrittore = medicoPrescrittore;
     }
 
+    //metodo per modificare tramite setter i dati della terapia
     public Boolean modificaDatiTerapia(int assunzioniGG, String indicazioni, int quantita, Farmaco farmaco, Diabetologo medicoPrescrittore){
         this.setAssunzioniGG(assunzioniGG);
         this.setFarmaco(farmaco);
@@ -68,6 +81,7 @@ public class Terapia {
         return true;
     }
 
+    //override del metodo toString adattato alla classe
     @Override
     public String toString() {
         String nomeFarmaco = (farmaco != null) ? farmaco.getNomeFarmaco() : "Non specificato";
