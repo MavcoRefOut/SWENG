c71509fa82322c686bd4cb89413f6eb93811a734
GiacomoM413 <132558826+Gieck413@users.noreply.github.com>
2026-09-16 16:40:25 +0200

    Update TerapiaConcomitante.java

--- ProgettoIng/src/main/java/org/example/TerapiaConcomitante.java
+++ ProgettoIng/src/main/java/org/example/TerapiaConcomitante.java
@@ -2,28 +2,32 @@ package org.example;
 
 import java.time.LocalDate;
 
+//classe che descrive una terapia condomitante
 public class TerapiaConcomitante extends Segnalazione{
     private String farmaco;
 
+    //costruttore di default della classe, usa quello di Segnalazione
     public TerapiaConcomitante() {
         super();
     }
-
-    public TerapiaConcomitante(LocalDate dataInizio, LocalDate dataFine,
-                               String note, String farmaco) {
+    
+    //costruttore con parametri della classe, usa quello di Segnalazione
+    public TerapiaConcomitante(LocalDate dataInizio, LocalDate dataFine, String note, String farmaco) {
         super(dataInizio, dataFine, note);
         this.farmaco = farmaco.trim();
     }
 
-
+    //getter del campo della classe omonimo
     public String getFarmaco() {
         return farmaco;
     }
-
+    
+    //setter del campo della classe omonimo
     public void setFarmaco(String farmaco) {
         this.farmaco = farmaco.trim();
     }
 
+    //override del metodo toString adattato alla classe
     @Override
     public String toString() {
         return "TerapiaConcomitante" +
