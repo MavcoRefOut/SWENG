ad13b82a3f12b068148a85502f577f3c1e99cb69
GiacomoM413 <132558826+Gieck413@users.noreply.github.com>
2026-09-16 16:30:03 +0200

    Update Persona.java

--- ProgettoIng/src/main/java/org/example/Persona.java
+++ ProgettoIng/src/main/java/org/example/Persona.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.util.Date;
 
+//classe che descrive una persona
 public class Persona {
     protected String codiceFiscale;
     protected String nome;
@@ -11,8 +12,10 @@ public class Persona {
     protected String username;
     protected String password;
 
+    //costruttore di default della classe
     public Persona(){};
 
+    //costruttore con parametri della classe
     public Persona(String codiceFiscale, String nome, String cognome, Date dataNascita,
                    String email, String username, String password) {
         this.setCodiceFiscale(codiceFiscale);
@@ -24,6 +27,7 @@ public class Persona {
         this.password = password;
     }
 
+    //setter del campo della classe omonimo, con controllo di validità
     public void setCodiceFiscale(String codiceFiscale) {
         if (codiceFiscale == null || codiceFiscale.trim().length() != 16) {
             throw new IllegalArgumentException("Il codice fiscale deve essere lungo esattamente 16 caratteri");
@@ -31,50 +35,62 @@ public class Persona {
         this.codiceFiscale = codiceFiscale.trim().toUpperCase();
     }
 
+     //setter del campo della classe omonimo
     public String getCodiceFiscale() {
         return codiceFiscale;
     }
 
+     //getter del campo della classe omonimo
     public String getNome() {
         return nome;
     }
 
+     //setter del campo della classe omonimo
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+     //getter del campo della classe omonimo
     public String getCognome() {
         return cognome;
     }
 
+     //setter del campo della classe omonimo
     public void setCognome(String cognome) {
         this.cognome = cognome;
     }
 
+     //getter del campo della classe omonimo
     public Date getDataNascita() {
         return dataNascita;
     }
 
+     //setter del campo della classe omonimo
     public void setDataNascita(Date dataNascita) {
         this.dataNascita = dataNascita;
     }
 
+     //getter del campo della classe omonimo
     public String getEmail() {
         return email;
     }
 
+     //setter del campo della classe omonimo
     public void setEmail(String email) {
         this.email = email;
     }
 
+     //getter del campo della classe omonimo
     public String getUsername() {
         return username;
     }
 
+     //setter del campo della classe omonimo
     public void setUsername(String username) {
         this.username = username;
     }
 
+     //getter del campo della classe omonimo
     public String getPassword() {
         return password;
     }
